cf06a8facfb06e02ee63e9e661cf2ce8e24c5986
Stephen <brokensteelstring@gmail.com>
2020-07-21 21:05:54 -0700

    Basic pathfinding AI setup
    Minions will follow the paths only to the destination. Need to work on adding movement variance between then so they aren't single file all the gad dang time

---
+++ Yard Wars/Assets/Scripts/AI/Minion AI/Pathfinding.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+    private GameObject target;
+    private NavMeshAgent navAgent;
+    public Vector3[] cornerPoints;
+    public Collider[] cols;
+    public int PF_Index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("EnemyTarget");
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.SetDestination(target.transform.position);
+        cornerPoints = navAgent.path.corners;
+        //navAgent.SetDestination(cornerPoints[0]);
+        //cornerPoints = VaryCorners(cornerPoints);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3[] VaryCorners(Vector3[] corners)
+    {
+        for (int i = 0; i < corners.Length; ++i)
+        {
+            cols = Physics.OverlapSphere(corners[i], 0.5f, 10);
+
+            Vector3 newPos = Vector3.Lerp(corners[i], cols[0].transform.position, Random.Range(0f, 1f));
+            corners[i] = newPos;
+            
+        }
+        return corners;
+    }
+}
