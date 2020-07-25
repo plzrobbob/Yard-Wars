91727e62d0ec2fbeab7de25cd39768317f9d8f6e
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-07-25 11:32:52 -0700

    added placeholder turret
    Turret will look at target

---
+++ Yard Wars/Assets/Scripts/Turret/Turret1Script/TurretController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public GameObject target;
+
+    public GameObject TurretHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion lookOnLook = Quaternion.LookRotation(target.transform.position - TurretHead.transform.position);
+
+        TurretHead.transform.rotation = Quaternion.Slerp(TurretHead.transform.rotation, lookOnLook, Time.deltaTime);
+    }
+}
