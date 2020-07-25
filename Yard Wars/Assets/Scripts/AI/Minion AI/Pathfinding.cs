0647af042d818e0764d5426214275b527a6be189
Stephen <brokensteelstring@gmail.com>
2020-07-25 03:13:13 -0700

    Basic pathfinding, navigate from spawn to goal along paths.

--- Yard Wars/Assets/Scripts/AI/Minion AI/Pathfinding.cs
+++ Yard Wars/Assets/Scripts/AI/Minion AI/Pathfinding.cs
@@ -17,27 +17,12 @@ public class Pathfinding : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("EnemyTarget");
         navAgent = GetComponent<NavMeshAgent>();
         navAgent.SetDestination(target.transform.position);
-        cornerPoints = navAgent.path.corners;
-        //navAgent.SetDestination(cornerPoints[0]);
-        //cornerPoints = VaryCorners(cornerPoints);
+        navAgent.radius = Random.Range(0.75f, 1.75f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    Vector3[] VaryCorners(Vector3[] corners)
-    {
-        for (int i = 0; i < corners.Length; ++i)
-        {
-            cols = Physics.OverlapSphere(corners[i], 0.5f, 10);
-
-            Vector3 newPos = Vector3.Lerp(corners[i], cols[0].transform.position, Random.Range(0f, 1f));
-            corners[i] = newPos;
-            
-        }
-        return corners;
+        cornerPoints = navAgent.path.corners;
     }
 }
