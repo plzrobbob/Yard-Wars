1a0f046eb89fde286371262e0f8784ba5a94c55f
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-08-01 12:55:54 -0700

    Initial Turret Agro System

--- Yard Wars/Assets/Scripts/Turret/Turret1Script/TurretController.cs
+++ Yard Wars/Assets/Scripts/Turret/Turret1Script/TurretController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TurretController : MonoBehaviour
@@ -7,17 +8,67 @@ public class TurretController : MonoBehaviour
     public GameObject target;
 
     public GameObject TurretHead;
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject[] Enemies;
+
+    [SerializeField] int range;
+
+    public GameObject PlaceHolderTarget;
+
+    private void Start()
     {
-        
+        target = PlaceHolderTarget;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Quaternion lookOnLook = Quaternion.LookRotation(target.transform.position - TurretHead.transform.position);
+        UpdateTarget();
 
+        GameObject tempTarget = null;
+        if (target == null || target == PlaceHolderTarget)
+        {
+            FindPossibleTarget();
+            tempTarget = FindTarget();
+            if (tempTarget != null)
+            {
+                target = tempTarget;
+                tempTarget = null;
+            }
+        }
+
+        Quaternion lookOnLook = Quaternion.LookRotation(target.transform.position - TurretHead.transform.position);
         TurretHead.transform.rotation = Quaternion.Slerp(TurretHead.transform.rotation, lookOnLook, Time.deltaTime);
+    }
+
+    public void UpdateTarget()
+    {
+        if (!target.activeInHierarchy && target != PlaceHolderTarget || Vector3.Distance(this.gameObject.transform.position, target.transform.position) > range)
+        {
+            target = PlaceHolderTarget;
+        }
+    }
+
+    public void FindPossibleTarget()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    public GameObject FindTarget()
+    {
+        float smallestrangeHolder = range;
+        GameObject temptarget = null;
+        for (int i = 0; i < Enemies.Length; i++)
+        {
+            float tempdist = Vector3.Distance(this.gameObject.transform.position, Enemies[i].transform.position);
+            if (tempdist < range && Enemies[i].activeInHierarchy)
+            {
+                if (tempdist < smallestrangeHolder)
+                {
+                    smallestrangeHolder = tempdist;
+                    temptarget = Enemies[i];
+                }
+            }
+        }
+        return temptarget;
     }
 }
