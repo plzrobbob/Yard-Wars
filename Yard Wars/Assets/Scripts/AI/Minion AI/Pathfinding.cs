75d080a8631ce43f2567ab67225ca4c38884dd38
Stephen <brokensteelstring@gmail.com>
2020-08-06 13:15:01 -0700

    Push for Cameron to grab my AI section

--- Yard Wars/Assets/Scripts/AI/Minion AI/Pathfinding.cs
+++ Yard Wars/Assets/Scripts/AI/Minion AI/Pathfinding.cs
@@ -7,8 +7,6 @@ public class Pathfinding : MonoBehaviour
 {
     private GameObject target;
     private NavMeshAgent navAgent;
-    public Vector3[] cornerPoints;
-    public Collider[] cols;
     public int PF_Index = 0;
 
     // Start is called before the first frame update
@@ -17,12 +15,14 @@ public class Pathfinding : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("EnemyTarget");
         navAgent = GetComponent<NavMeshAgent>();
         navAgent.SetDestination(target.transform.position);
-        navAgent.radius = Random.Range(0.75f, 1.75f);
+        navAgent.acceleration = Random.Range(2.5f, 4f);
+        navAgent.angularSpeed = Random.Range(0.5f, 1.5f);
+        navAgent.stoppingDistance = Random.Range(0.5f, 2.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        cornerPoints = navAgent.path.corners;
+
     }
 }
