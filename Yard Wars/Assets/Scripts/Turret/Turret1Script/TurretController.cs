7bc3eb568dbb91d1cfa4aafbe28161bd1b9493ff
Benjamin Alexander <005886968@coyote.csusb.edu>
2020-08-16 10:35:51 -0700

    Fixed Turret Target Acquisition

--- Yard Wars/Assets/Scripts/Turret/Turret1Script/TurretController.cs
+++ Yard Wars/Assets/Scripts/Turret/Turret1Script/TurretController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,130 +8,90 @@ public class TurretController : MonoBehaviour
 {
     public GameObject target;
 
-    public GameObject TurretHead;
+    public int range;
 
-    public GameObject[] Enemies;
+    public string EnemyTag;
 
-    [SerializeField] int range;
+    public Transform PartToRotate;
 
-    public GameObject PlaceHolderTarget;
+    public float TurnSpeed = 5f;
 
-    [SerializeField] float shootspeed;
-    [SerializeField] float turretHeadSpeed;
+    public GameObject PlaceHoldertarget;
 
-    private Coroutine co;
-    private bool coRunning;
-
-    public GameObject projectile;
-    public GameObject bulletstartpos;
-
-    private bool finishedaiming;
-
-    private void Start()
+    void Start()
     {
-        target = PlaceHolderTarget;
-        co = null;
+        InvokeRepeating("UpdateTarget", 0f, .5f );
     }
 
-    void Update()
+    void UpdateTarget()
     {
-        //if the target is dead or out of range then change target back to palceholder target
-        if (target.activeSelf == false ||target == null || Vector3.Distance(this.gameObject.transform.position, target.transform.position) > range || target.GetComponent<HealthScript>().CurrentHealth <= 0)
-        {
-            target = PlaceHolderTarget;
-        }
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
 
-        //attemp to see if target is null
-        if (!target || target == null || target.Equals(null) || target.gameObject.Equals(null) || target.activeInHierarchy == false || target.activeSelf == false || target.gameObject == null)
-        {
-            Debug.Log("null");
-        }
+        float shortestDitance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
 
-        GameObject tempTarget = null;
-        //if the target has not been set or is null then fin an array of gameojbects and see if the enemy is in the turrets range
-        if (target.activeSelf == false || target == PlaceHolderTarget)
-        {
-            //find enemies and add them to the enemy array 
-            Enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
-            //set the target ot the nearest enemy in range of the turret
-            tempTarget = FindTarget();
-            if (tempTarget != null)
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDitance)
             {
-                target = tempTarget;
-                tempTarget = null;
-            }
-            else 
-            {
-                target = PlaceHolderTarget;
+                shortestDitance = distanceToEnemy;
+                nearestEnemy = enemy;
             }
         }
-        Vector3 temp = target.transform.position;
-        temp.y += .5f;
-        Quaternion lookOnLook = Quaternion.LookRotation(temp - TurretHead.transform.position);
-        //change the turret head rotation to face the target
-        if (target.activeSelf != false)
+
+        if (target != null && Vector3.Distance(transform.position, target.transform.position) > range)
         {
-            TurretHead.transform.rotation = Quaternion.Slerp(TurretHead.transform.rotation, lookOnLook, turretHeadSpeed * Time.deltaTime);
+            target = null;
         }
 
-        Debug.Log(lookOnLook);
-        Debug.Log(TurretHead.transform.localRotation);
-
-        if (lookOnLook.y -.05f <= TurretHead.transform.localRotation.y && TurretHead.transform.localRotation.y <= lookOnLook.y+.05f)
+        if (nearestEnemy != null && shortestDitance <= range)
         {
-            finishedaiming = true;
-        }
-        else 
-        {
-            finishedaiming = false;
-        }
-
-        //if a target is found begin shooting
-        if ((target.activeSelf != false && target != null && target != PlaceHolderTarget) && !coRunning && finishedaiming)
-        {
-            coRunning = true;
-            co = StartCoroutine(TurretFire());
-        }
-
-        if (co != null)
-        {
-            //if no taget is found then dont shoot
-            if (((target.activeSelf == false || target == null || target == PlaceHolderTarget) && coRunning) || !finishedaiming)
+            if (target == null)
             {
-                coRunning = false;
-                StopCoroutine(co);
-                co = null;
-            }
+                target = nearestEnemy;
+            } 
+        }
+        else
+        {
+            target = null;
         }
     }
 
-    public GameObject FindTarget()
+    private void Update()
     {
-        float smallestrangeHolder = range;
-        GameObject temptarget = null;
-        for (int i = 0; i < Enemies.Length; i++)
+        //if (target == null)
+        //{
+        //    return;
+        //}
+        try
         {
-            float tempdist = Vector3.Distance(this.gameObject.transform.position, Enemies[i].transform.position);
-            if (tempdist < range && Enemies[i].activeInHierarchy && Enemies[i].GetComponent<HealthScript>().CurrentHealth > 0)
+            if (target.GetComponent<HealthScript>().CurrentHealth <= 0)
             {
-                if (tempdist < smallestrangeHolder)
-                {
-                    smallestrangeHolder = tempdist;
-                    temptarget = Enemies[i];
-                }
+                target = null;
             }
         }
-        return temptarget;
+        catch { }
+
+        if (target != null)
+        {
+            Vector3 dir = (target.transform.position - transform.position);
+            Quaternion LookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, LookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
+            PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+        else
+        {
+            Vector3 dir = (PlaceHoldertarget.transform.position - transform.position);
+            Quaternion LookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(PartToRotate.rotation, LookRotation, Time.deltaTime * TurnSpeed).eulerAngles;
+            PartToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
     }
 
-    public IEnumerator TurretFire()
+    private void OnDrawGizmosSelected()
     {
-        while (target.activeSelf != false || PlaceHolderTarget)
-        {
-            GameObject go = Instantiate(projectile, bulletstartpos.transform.position, transform.rotation);
-            go.GetComponent<TurretBullet1>().Weapon = TurretHead;
-            yield return new WaitForSeconds(shootspeed);
-        }
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
