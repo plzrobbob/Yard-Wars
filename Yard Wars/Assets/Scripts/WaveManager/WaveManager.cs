08ef39f83f8eff28a19f887bbedf1554aa7e450c
Christian Morales <Chrismorales116@gmail.com>
2020-10-20 09:26:40 -0700

    Commented the shit outta my code
    The Wave Manager script is full of comments now bby

--- Yard Wars/Assets/Scripts/WaveManager/WaveManager.cs
+++ Yard Wars/Assets/Scripts/WaveManager/WaveManager.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/* Description: The point of this script is to spawn enemies on each wave. This also means it needs to determine when a wave is
+ * going on or not.
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,35 +16,32 @@ public class WaveManager : MonoBehaviour
     GameObject currEnemyPrefab; // The most current enemy prefab, meant to be overwritten at each iteration
     GameObject newFirstNode; // The first node for the pathfinding script to follow along a path for each enemy
 
-    int waveNumber;
-    int waveAmount;
+    int waveNumber; //This int corrolates to the number of waves that have gone by
+    int waveAmount; //The amount of minions spawned during the current wave
 
-    public GameObject minionPrefabTypeOne;
-    public GameObject minionPrefabTypeTwo;
-    public GameObject minionPrefabTypeThree;
-    public GameObject minionPrefabTypeFour;
-    public GameObject minionPrefabTypeFive;
+    public GameObject minionPrefabTypeOne; //The prefab for enemy type 1 which is: TBD
+    public GameObject minionPrefabTypeTwo; //The prefab for enemy type 2 which is: TBD
+    public GameObject minionPrefabTypeThree; //The prefab for enemy type 3 which is: TBD
+    public GameObject minionPrefabTypeFour; //The prefab for enemy type 4 which is: TBD
+    public GameObject minionPrefabTypeFive; //The prefab for enemy type 5 which is: TBD
 
-    private int spawn; // n is equal to spawn n 
-                        // 1 is equal to spawn 1, 2 is equal to spawn 2, 3 is equal to spawn 3, 4 random pick spawner
+    private int spawn; //This value correlates to the spawn location used in the current wave. If the number goes above the amount
+                       //of spawn locations then enemies spawn in random locations.
 
     // Start is called before the first frame update
     void Start()
     {
+        //Setting up all of our values how we'd like them on startup:
         ready4Wave = true;
-
         waveInProgress = false;
-
         spawn = 0;
         waveNumber = 1;
-
-        //Instantiate(minionPrefabTypeOne, spawnPointList[0], Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Check if the player has pressed ready. If they have call the wave function
+        //Check if the player has pressed the ready key. If they have, call the wave function
         if (Input.GetKeyDown("r"))
         {
             //Check to make sure wave is not in progress
@@ -47,10 +49,11 @@ public class WaveManager : MonoBehaviour
 
             if (ready4Wave == true && waveInProgress == false)
             {
+                //flipping the bools once a wave is in progress
                 ready4Wave = false;
                 waveInProgress = true;
 
-                Wave();
+                Wave(); //The function that actually runs a wave
             }
 
         }
@@ -61,11 +64,9 @@ public class WaveManager : MonoBehaviour
             DestroyAllEnemies();
         }
 
-        //check if wave is done with GameObject.FindGameObjectsWithTag("Enemy").Length
-
     }
 
-    void WaveCheck()
+    void WaveCheck() //This function checks how many enemies are on screen. If there are none then it flips the ready4wave bool.
     {
         if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
         {
@@ -77,23 +78,33 @@ public class WaveManager : MonoBehaviour
     void Wave()
     {
 
-        //If wave is ready to start check the wave number and get the amount of minions for that wave using the minion count algorithm:
-
+        //If wave is ready to start, check the wave number and get the amount of minions for that wave using the minion count
+        //algorithm:
         waveAmount = waveNumber * Random.Range(5, 10);
+
         spawn++;
-        StartCoroutine(Spawner());
+        StartCoroutine(Spawner()); //Actually spawning the enemies is handled by this Coroutine
     }
 
-    IEnumerator Spawner()
+    IEnumerator Spawner() //Coroutine that actually handles spawning enemies
     {
         //Debug.Log("Coroutine started, bitch!");
-        if (spawn > spawnPointList.Count)
-        {
-            for (int i = 0; i < waveAmount; i++)         //loop until the amount of minions for this wave have spawned
-            {
-                GameObject tempSpawnObject = spawnPointList[Random.Range(0, spawnPointList.Count)];
-                Vector3 tempSpawnLocation = tempSpawnObject.transform.position;
 
+        if (spawn > spawnPointList.Count) //This condition is for when enemies spawn from random Spawn Points
+        {
+            for (int i = 0; i < waveAmount; i++) //loop until the amount of minions for this wave have spawned
+            {
+                GameObject tempSpawnObject = spawnPointList[Random.Range(0, spawnPointList.Count)]; //Creating a temporary GameObject
+                                                                                            //for the purpose of holding the Spawn
+                                                                                            //location info of the most recently 
+                                                                                            //spawned enemy
+                                                                                                    
+                Vector3 tempSpawnLocation = tempSpawnObject.transform.position; //The spawn location of the most recently spawned
+                                                                                //enemy.
+
+                //The purpose of this switch case is to tell the enemy's AI which pathing nodes to follow depending on the Spawn Point
+                //they've been assigned. This switch case was built for a map with 3 spawn points, if a map with more than 3 is used
+                //then modify this code to add more cases.
                 switch (tempSpawnObject == spawnPointList[0] ? "1" : tempSpawnObject == spawnPointList[1] ? "2" : tempSpawnObject == spawnPointList[2] ? "3" : "Other")
                 {
                     case "1":
@@ -110,19 +121,24 @@ public class WaveManager : MonoBehaviour
                         break;
                 }
 
-                currEnemyPrefab = Instantiate(minionPrefabTypeOne, tempSpawnLocation, Quaternion.identity); //spawn a specified amount of minions at a random spawn point
-                currEnemyPrefab.GetComponent<Pathfinding>().firstNode = newFirstNode;
-                yield return new WaitForSeconds(0.1f);
+                currEnemyPrefab = Instantiate(minionPrefabTypeOne, tempSpawnLocation, Quaternion.identity); //Spawns an enemy
+                currEnemyPrefab.GetComponent<Pathfinding>().firstNode = newFirstNode; //Sets the currently spawned enemy's first 
+                                                                                      //pathfinding node
+                yield return new WaitForSeconds(0.1f); //This is the time between enemy spawns
                 //Debug.Log("Holy fuck my guy, did you fucking see that shit spawn in a fucking random location???");
             }
         }
         else
         {
-            for (int i = 0; i < waveAmount; i++)        //loop until the amount of minions for this wave have spawned
+            for (int i = 0; i < waveAmount; i++) //loop until the amount of minions for this wave have spawned
             {
-                GameObject tempSpawnObject = spawnPointList[spawn - 1];
-                Vector3 tempSpawnLocation = tempSpawnObject.transform.position;
+                GameObject tempSpawnObject = spawnPointList[spawn - 1]; //Spawn location in this loop is determined wave number
+                Vector3 tempSpawnLocation = tempSpawnObject.transform.position; //The spawn location of the most recently spawned
+                                                                                //enemy.
 
+                //The purpose of this switch case is to tell the enemy's AI which pathing nodes to follow depending on the Spawn Point
+                //they've been assigned. This switch case was built for a map with 3 spawn points, if a map with more than 3 is used
+                //then modify this code to add more cases.
                 switch (tempSpawnObject == spawnPointList[0] ? "1" : tempSpawnObject == spawnPointList[1] ? "2" : tempSpawnObject == spawnPointList[2] ? "3" : "Other")
                 {
                     case "1":
@@ -139,28 +155,29 @@ public class WaveManager : MonoBehaviour
                         break;
                 }
 
-                currEnemyPrefab = Instantiate(minionPrefabTypeOne, tempSpawnLocation, Quaternion.identity);
-                currEnemyPrefab.GetComponent<Pathfinding>().firstNode = newFirstNode;
-                yield return new WaitForSeconds(0.1f);
+                currEnemyPrefab = Instantiate(minionPrefabTypeOne, tempSpawnLocation, Quaternion.identity); //Spawns an enemy
+                currEnemyPrefab.GetComponent<Pathfinding>().firstNode = newFirstNode;//Sets the currently spawned enemy's first 
+                                                                                     //pathfinding node
+                yield return new WaitForSeconds(0.1f); //This is the time between enemy spawns
                 //Debug.Log("Holy fuck my guy, did you fucking see that shit spawn in a totally NOT fucking random location???");
             }
         }
 
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f); //why did I put this here?
         //Debug.Log("Bool flipped motherfucker!");
-        waveInProgress = false;
-        waveNumber++;
+        waveInProgress = false; //If we've reached this part of the code then the spawning is over so we should flip this bool
+        waveNumber++; //Increment the wave number now that the wave has ended
     }
 
-    void DestroyAllEnemies()
+    void DestroyAllEnemies() //A debug function to kill off all enemies on the map
     {
 
-        GameObject[] enemyList = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] enemyList = GameObject.FindGameObjectsWithTag("Enemy"); //Create a list of every GameObject tagged "Enemy"
 
         for (var i = 0; i < enemyList.Length; i++)
         {
-            Destroy(enemyList[i]);
+            Destroy(enemyList[i]); //DESTROY EVERY FUCKER ON THAT LIST
         }
     }
 
