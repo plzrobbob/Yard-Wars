aa79d774097d95dcac60faf8a0bb32b546b76473
tintinjokes <tintinjokes13@hotmail.com>
2020-11-13 21:26:17 -0800

    Auto stash before merge of "TintinBranch" and "origin/Develop2.0"

---
+++ Yard Wars/Assets/Scripts/UI/MainMenuLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLoad : MonoBehaviour
+{
+    public GameObject immortal;
+    public bool isLoaded = false;
+    public void LoadGame()
+    {
+        isLoaded = true;
+        DontDestroyOnLoad(immortal);
+    }
+}
