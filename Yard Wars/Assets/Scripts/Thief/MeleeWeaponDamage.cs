34c70509b4970cadf58e14eb05840e0b8d8c2043
Christian Morales <chrismorales116@gmail.com>
2020-12-06 12:32:50 -0800

    Staging changes to switch branches
    idk what I even changed tbh

--- Yard Wars/Assets/Scripts/Thief/MeleeWeaponDamage.cs
+++ Yard Wars/Assets/Scripts/Thief/MeleeWeaponDamage.cs
@@ -10,7 +10,6 @@ public class MeleeWeaponDamage : MonoBehaviour
 
     public LayerMask EnemyMask;
     public Collider[] enemiesHit;
-    public int numberLETSFUCKINGGOOOOOOBOYYYYSSSSS;
 
 
     // Start is called before the first frame update
@@ -26,6 +25,8 @@ public class MeleeWeaponDamage : MonoBehaviour
         {
             attackSuccessful = false;
         }
+
+        OnDrawGizmos();
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -38,16 +39,31 @@ public class MeleeWeaponDamage : MonoBehaviour
             enemiesHit = Physics.OverlapSphere(transform.position, range, EnemyMask);
             Debug.Log("enemiesHit = " + EnemyMask);
 
+            DoDamage();
 
             //add code here to cause damage to whatever enters the trigger hitbox
-            HealthScript M_HealthScript = collider.gameObject.GetComponent<HealthScript>();
-            M_HealthScript.DamageHandler();
-            M_HealthScript.CurrentHealth -= damageAmount;
-
+            //HealthScript M_HealthScript = collider.gameObject.GetComponent<HealthScript>();
+            //M_HealthScript.DamageHandler();
+            //M_HealthScript.CurrentHealth -= damageAmount;
             attackSuccessful = true;
             Debug.Log("Sir, an iceburg struck the Trigger collider, I believe the ship is going down.");
         } 
 
 
+    }
+
+    void DoDamage()
+    {
+        for (int i = 0; i < enemiesHit.Length; i++)
+        {
+            HealthScript E_HealthScript = enemiesHit[i].gameObject.GetComponent<HealthScript>();
+            E_HealthScript.CurrentHealth -= damageAmount;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, range);
     }
 }
